76f3dc9493830c30ad0f675d3bb3195702a2d86b
emojs2013 <weiweo900504@gmail.com>
2013-07-18 16:26:15 +0800

    first commit

---
+++ examples/index.md
@@ -0,0 +1,9 @@
+# 演示文档
+
+---
+
+````javascript
+seajs.use('animation', function(example){
+
+});
+````
---
+++ tests/example-spec.js
@@ -0,0 +1,12 @@
+define(function(require) {
+
+  var example = require('../src/example');
+
+  describe('example', function() {
+
+    it('normal usage', function() {
+
+    });
+  });
+
+});
